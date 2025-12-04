decaa0a98e7db63757f4dabc30fcaf6de802ef17
Yusuke Doi <yuusukedoi23@gmail.com>
2025-12-04 15:57:33 +0900

    Add config exception reporter

--- server.rb
+++ server.rb
@@ -3,6 +3,12 @@ require 'optparse'
 require_relative './tools/search_files'
 require_relative './tools/read_file_content'
 
+MCP.configure do |config|
+  config.exception_reporter = ->(exception, server_context) {
+    $stderr.puts exception.full_message
+  }
+end
+
 options = {}
 opt = OptionParser.new
 opt.on('--repo_dir VAL') {|v| options[:repo_dir] = v }
